8f3309bbf6c083745a6626207e298864c3225df4
Ashish Daga <jain.ashish12345@gmail.com>
2021-07-08 00:40:45 +0530

    commit msg

---
+++ src/test/java/MainProject/MavenJava/AutomationAppium.java
@@ -0,0 +1,13 @@
+package MainProject.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AutomationAppium {
+
+	@Test
+	public void Demo2() {
+		System.out.println("Hi I'm your project 2");
+	}
+	
+}
+
